0abf70b1d49f3408d991b6295786e5ef6995f2e8
J <09.chen@gmail.com>
2024-05-07 12:35:02 -0700

    Redo Navbar using bulma

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// npx tailwindcss -i ./index.css -o ./index/css --watch
